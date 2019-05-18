a19109a755e57f789b58ea02a93f5d69305fafc9
Andrew <AndrewYowenHuang@gmail.com>
2019-05-18 17:09:22 -0400

    Fibonacci

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,47 @@
 package com.techelevator;
+import java.util.Scanner;
 
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		/*
+		The Fibonacci numbers are the integers in the following sequence:
 
+		    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+
+		By definition, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent 
+		number is the sum of the previous two.
+
+		Write a command line program which prompts the user for an integer value and display the Fibonacci 
+		sequence leading up to that number.
+
+		```
+		Please enter the Fibonacci number: 25
+
+		0, 1, 1, 2, 3, 5, 8, 13, 21
+		```
+		*/
+		
+		Scanner console = new Scanner(System.in);
+	    System.out.print("Please enter the Fibonacci number: ");
+	    int n = console.nextInt();
+	    System.out.println("");
+	    
+
+	    System.out.print("0, 1, 1, ");
+	    int f1 = 1;
+	    int f2 = 1;
+	    for(int i = 1; i + f2 < n;) {
+	        f1 = i + f2;
+	        i = f2;
+            f2 = f1;
+            System.out.print(f1);
+            if (i + f2 < n) {
+            	System.out.print(", ");
+            }
+            else {
+            	break;
+            }
+	    } 
 	}
-
 }
--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/TempConvert.java
@@ -4,25 +4,6 @@ import java.util.Scanner;
 public class TempConvert {
 
 	public static void main(String[] args) {
-		/*
-		The Fahrenheit to Celsius conversion formula is:
-
-		    Tc = (Tf - 32) / 1.8
-
-		where 'Tc' is the temperature in Celsius, and 'Tf' is the temperature in Fahrenheit
-
-		The Celsius to Fahrenheit conversion formula is:
-
-		    Tf = Tc * 1.8 + 32
-
-		Write a command line program which prompts a user to enter a temperature, and whether its in degrees (C)elsius or (F)arenheit. Convert the temperature to the opposite degrees, and display the old and new temperatures to the console.
-
-		```
-		Please enter the temperature: 58
-		Is the temperature in (C)elcius, or (F)arenheit? F
-		58F is 14C.
-		```
-		*/
 		Scanner input = new Scanner(System.in);
 		System.out.print("Please enter the temperature: ");
 		int temp = input.nextInt();
