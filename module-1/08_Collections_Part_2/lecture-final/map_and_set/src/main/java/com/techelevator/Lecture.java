bc9d20548639aa66e67032cb063c48c5d2cc6d0e
Brian Lauvray <lauvray.1@osu.edu>
2019-05-21 22:26:00 -0400

    Added Collections Part 2

---
+++ module-1/08_Collections_Part_2/lecture-final/map_and_set/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,66 @@
+package com.techelevator;
+
+
+public class Lecture {
+
+	public static void main(String[] args) {
+
+		System.out.println("####################");
+		System.out.println("       MAPS");
+		System.out.println("####################");
+		System.out.println();
+		
+		/* DECLARING AND INSTANTIATING A MAP */
+
+		
+		/* ADDING ITEMS TO A MAP */
+
+		
+		/* UPDATING AN ITEM IN A MAP */
+
+
+		/* RETRIEVING AN ITEM FROM A MAP */
+
+		
+		
+		/* REMOVING AN ITEM FROM A MAP */
+
+		
+		
+		/* CHECK IF AN ITEM EXISTS */
+
+		
+		
+		System.out.println();
+		/* LOOPING OVER A MAP */
+		
+
+		
+		
+		System.out.println();
+		/* THE MAP KEY AND VALUE DATA TYPES */
+
+		
+					
+		System.out.println("####################");
+		System.out.println("       SETS");
+		System.out.println("####################");
+		System.out.println();
+
+		/* DECLARE AND INSTANTIATE A HashSet */
+					
+		
+		/* ADD ITEMS TO THE SET */
+
+		
+		/* LOOP OVER A SET */
+
+		
+		System.out.println();
+		/* USE A SET TO REMOVE DUPLICATES AND ORDER THE ARRAY */
+		String[] instructors = {"Rachelle", "John", "Andrew", "John", "Stephanie", "Rachelle", "Steve", "John", "Steve" };
+		
+
+	}
+
+}
