9ba3ca7965770218d2373bf14acc06aa28d6a9be
Andrew <AndrewYowenHuang@gmail.com>
2019-05-26 21:44:12 -0400

    HomeworkAssignment Complete

--- module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,50 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+	
+	private int possibleMarks;
+	private int totalMarks;
+	private String submitterName;
+	
+	public HomeworkAssignment(int possibleMarks) {
+		this.possibleMarks = possibleMarks;
+	}
+	
+	public String getLetterGrade() {
+		if (((double) totalMarks / possibleMarks) * 100 >= 90) {
+			return "A";
+		}
+		else if (((double) totalMarks / possibleMarks) * 100 >= 80) {
+			return "B";
+		}
+		else if (((double) totalMarks / possibleMarks) * 100 >= 70) {
+			return "C";
+		}
+		else if (((double) totalMarks / possibleMarks) * 100 >= 60) {
+			return "D";
+		}
+		else {
+			return "F";
+		}
+	}
+	
+	public int getTotalMarks() {
+		return totalMarks;
+	}
 
+	public void setTotalMarks(int totalMarks) {
+		this.totalMarks = totalMarks;
+	}
+	
+	public String getSubmitterName() {
+		return submitterName;
+	}
+
+	public void setSubmitterName(String submitterName) {
+		this.submitterName = submitterName;
+	}
+
+	public int getPossibleMarks() {
+		return possibleMarks;
+	}
 }
