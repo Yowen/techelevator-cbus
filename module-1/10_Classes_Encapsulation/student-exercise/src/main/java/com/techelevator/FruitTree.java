a67695e1e58d74e90071e1e7e3e2bef96f449a90
Andrew <AndrewYowenHuang@gmail.com>
2019-05-28 09:36:54 -0400

    Fruit tree assignment complete

--- module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/FruitTree.java
+++ module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/FruitTree.java
@@ -2,4 +2,28 @@ package com.techelevator;
 
 public class FruitTree {
 
+	private String typeOfFruit;
+	private int piecesOfFruitLeft;
+	
+	public String getTypeOfFruit() {
+		return typeOfFruit;
+	}
+	public int getPiecesOfFruitLeft() {
+		return piecesOfFruitLeft;
+	}
+	
+	public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+		this.typeOfFruit = typeOfFruit;
+		this.piecesOfFruitLeft = startingPiecesOfFruit;
+	}
+	
+	public boolean pickFruit(int numberOfPiecesToRemove) {
+		if (piecesOfFruitLeft > 0) {
+			piecesOfFruitLeft -= numberOfPiecesToRemove;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
