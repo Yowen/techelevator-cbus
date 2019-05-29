ef0f4155e261e0b7bc4b00124e04c80548faf111
Andrew <AndrewYowenHuang@gmail.com>
2019-05-29 10:09:13 -0400

    Finished Encapsulation Assignment

--- module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Elevator.java
+++ module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Elevator.java
@@ -1,6 +1,45 @@
 package com.techelevator;
 
 public class Elevator {
-
+	private int currentFloor;
+	private int numberOfFloors;
+	private boolean doorOpen;
 	
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+	public int getNumberOfFloors() {
+		return numberOfFloors;
+	}
+	public boolean isDoorOpen() {
+		return doorOpen;
+	}
+	public Elevator(int totalNumberOfFloors) {
+		numberOfFloors = totalNumberOfFloors;
+		currentFloor = 1;
+	}
+	public void openDoor() {
+		doorOpen = true;
+	}
+	public void closeDoor() {
+		doorOpen = false;
+	}
+	public void goUp(int desiredFloor) {
+		if (doorOpen == false){
+			if (desiredFloor <= numberOfFloors) {
+				if (desiredFloor > currentFloor) {
+					currentFloor = desiredFloor;
+				}
+			}
+		}
+	}
+	public void goDown(int desiredFloor) {
+		if (doorOpen == false) {
+			if (desiredFloor >= 1 ) {
+				if (desiredFloor < currentFloor){
+					currentFloor = desiredFloor;
+				}
+			}
+		}
+	}
 }
---
+++ module-1/12_Polymorphism/student-lecture/src/main/java/com/techelevator/farm/Tractor.java
@@ -0,0 +1,9 @@
+package com.techelevator.farm;
+
+public class Tractor extends FarmAnimal {
+
+	public Tractor() {
+		super("Tractor", "Rrrrrr!", 20);
+	}
+	
+}
