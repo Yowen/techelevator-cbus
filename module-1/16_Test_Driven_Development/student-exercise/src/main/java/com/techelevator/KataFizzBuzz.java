baf91f183b45236ab5f5a7cf2bad0d41491f435e
Andrew <AndrewYowenHuang@gmail.com>
2019-06-03 16:24:59 -0400

    KataFizzBuzz

--- module-1/16_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -3,7 +3,32 @@ package com.techelevator;
 public class KataFizzBuzz {
 
 	public String fizzBuzz(int number) {
-		return null;
+		
+		String input = Integer.toString(number);
+		
+		if (number > 100 || number < 1) {
+			return "";
+		}
+		else if (number % 3 == 0 && number % 5 == 0) {
+			return "FizzBuzz";
+		}
+		else if (number % 3 == 0 && number % 5 != 0) {
+			return "Fizz";
+		}
+		else if (number % 5 == 0 && number % 3 != 0) {
+			return "Buzz";
+		}
+		else if (input.contains("3") && input.contains("5")) {
+			return "FizzBuzz";
+		}
+		else if (input.contains("3")) {
+			return "Fizz";
+		}
+		else if (input.contains("5")) {
+			return "Buzz";
+		}
+		else {
+			return Integer.toString(number);
+		}
 	}
-	
 }
--- module-1/16_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -16,9 +16,63 @@ public class KataFizzBuzzTest {
 	public void divisible_by_3() {
 		// Arrange
 		// Act
-		String output = kataFizzBuzz.fizzBuzz(3);
+		String result = kataFizzBuzz.fizzBuzz(3);
 		// Assert
-		Assert.assertEquals("1", output);
+		Assert.assertEquals("Fizz", result);
+	}
+	
+	@Test
+	public void divisible_by_5() {
+		// Arrange
+		// Act
+		String result = kataFizzBuzz.fizzBuzz(5);
+		// Assert
+		Assert.assertEquals("Buzz", result);
+	}
+	
+	@Test
+	public void divisible_by_3_and_5() {
+		// Arrange
+		// Act
+		String result = kataFizzBuzz.fizzBuzz(15);
+		// Assert
+		Assert.assertEquals("FizzBuzz", result);
+	}
+	
+	@Test
+	public void is_number_0() {
+		// Arrange
+		// Act
+		String result = kataFizzBuzz.fizzBuzz(0);
+		// Assert
+		Assert.assertEquals("", result);
+	}
+	
+	@Test
+	public void is_number_between_1_and_100() {
+		// Arrange
+		// Act
+		String result = kataFizzBuzz.fizzBuzz(22);
+		// Assert
+		Assert.assertEquals("22", result);
+	}
+	
+	@Test
+	public void or_contains_3() {
+		String result = kataFizzBuzz.fizzBuzz(13);
+		Assert.assertEquals("Fizz", result);
+	}
+	
+	@Test
+	public void or_contains_5() {
+		String result = kataFizzBuzz.fizzBuzz(52);
+		Assert.assertEquals("Buzz", result);
+	}
+	
+	@Test
+	public void or_contains_3_and_5() {
+		String result = kataFizzBuzz.fizzBuzz(53);
+		Assert.assertEquals("FizzBuzz", result);
 	}
 	
 }
