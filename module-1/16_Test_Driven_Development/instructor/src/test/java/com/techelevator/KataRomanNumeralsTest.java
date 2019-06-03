426f5613d688d629c7a6700bf977b85152a45d86
Brian <brian@techelevator.com>
2019-06-03 12:21:19 -0400

    TDD class code

--- module-1/16_Test_Driven_Development/instructor/src/test/java/com/techelevator/KataRomanNumeralsTest.java
+++ module-1/16_Test_Driven_Development/instructor/src/test/java/com/techelevator/KataRomanNumeralsTest.java
@@ -5,4 +5,25 @@ import static org.junit.Assert.*;
 
 public class KataRomanNumeralsTest {
 
+	private KataRomanNumerals romanNumerals;
+	
+	@Before
+	public void setup() {
+		romanNumerals = new KataRomanNumerals();
+	}
+	
+	@Test
+	public void one_characters_numbers() {
+		int num = romanNumerals.convertToDigit("I");
+		Assert.assertEquals("Incorrect value for I", 1, num);
+		
+		num = romanNumerals.convertToDigit("V");
+		Assert.assertEquals("Incorrect value for V", 5, num);
+	}
+	
+	@Test
+	public void two_character_numbers_that_are_not_groups() {
+		int num = romanNumerals.convertToDigit("II");
+		Assert.assertEquals("Incorrect value for II", 2, num);
+	}
 }
