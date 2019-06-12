bb514ee83e414d778e01b10033cb68d59d7e0964
Andrew <AndrewYowenHuang@gmail.com>
2019-06-12 09:32:48 -0400

    Progress on Tue HW

--- module-2/02_Aggregate_Functions_and_GROUP_BY/student-exercise/aggregate-functions-exercises.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/student-exercise/aggregate-functions-exercises.sql
@@ -21,57 +21,88 @@ ORDER BY district ASC, name ASC
 -- country code should be returned as a single column named country_and_code 
 -- and should contain values such as ‘Angola (AGO)’ 
 -- (58 rows)
+SELECT (name || ' (' || code || ')') AS name_and_code, region FROM country
+WHERE continent = 'Africa';
 
 -- 3. The per capita GNP (i.e. GNP multipled by 1000000 then divided by population) of all countries in the 
 -- world sorted from highest to lowest. Recall: GNP is express in units of one million US Dollars 
 -- (highest per capita GNP in world: 37459.26)
+SELECT (gnp * 1000000 / population) AS per_capita_gnp FROM country
+WHERE population > 0
+ORDER BY per_capita_gnp DESC;
 
 -- 4. The average life expectancy of countries in South America.
 -- (average life expectancy in South America: 70.9461)
+SELECT AVG(lifeexpectancy) FROM country
+WHERE continent = 'South America';
 
 -- 5. The sum of the population of all cities in California.
 -- (total population of all cities in California: 16716706)
+SELECT SUM(population) FROM city
+WHERE district = 'California';
 
 -- 6. The sum of the population of all cities in China.
 -- (total population of all cities in China: 175953614)
+SELECT SUM(population) FROM city
+WHERE countrycode = 'CHN';
 
 -- 7. The maximum population of all countries in the world.
 -- (largest country population in world: 1277558000)
+SELECT MAX(population) FROM country;
 
 -- 8. The maximum population of all cities in the world.
 -- (largest city population in world: 10500000)
+SELECT MAX(population) FROM city;
 
 -- 9. The maximum population of all cities in Australia.
 -- (largest city population in Australia: 3276207)
+SELECT MAX(population) FROM city
+WHERE countrycode IN (SELECT code FROM country WHERE name = 'Australia');
 
 -- 10. The minimum population of all countries in the world.
 -- (smallest_country_population in world: 50)
+SELECT MIN(population) FROM country
+WHERE population > 0;
 
 -- 11. The average population of cities in the United States.
 -- (avgerage city population in USA: 286955.3795)
+SELECT AVG(population) FROM city
+WHERE population > 0 and countrycode = 'USA';
 
 -- 12. The average population of cities in China.
 -- (average city population in China: 484720.6997 approx.)
+SELECT AVG(population) FROM city
+WHERE countrycode = 'CHN';
 
 -- 13. The surface area of each continent ordered from highest to lowest.
 -- (largest continental surface area: 31881000, "Asia")
+SELECT SUM(surfacearea) FROM country
+GROUP BY continent;
 
 -- 14. The highest population density (population divided by surface area) of all 
 -- countries in the world. 
 -- (highest population density in world: 26277.7777)
+SELECT MAX(population / surfacearea) AS highest_population_density FROM country;
 
 -- 15. The population density and life expectancy of the top ten countries with the 
 -- highest life expectancies in descending order. 
 -- (highest life expectancies in world: 83.5, 166.6666, "Andorra")
+SELECT name, (population / surfacearea) AS population_density, lifeexpectancy FROM country
+WHERE lifeexpectancy IS NOT NULL
+ORDER BY lifeexpectancy DESC
+LIMIT 10;
 
 -- 16. The difference between the previous and current GNP of all the countries in 
 -- the world ordered by the absolute value of the difference. Display both 
 -- difference and absolute difference.
 -- (smallest difference: 1.00, 1.00, "Ecuador")
+SELECT (gnp - gnpold) AS gnp_difference, ABS(gnp - gnpold) AS abs_gnp_difference, name FROM country
+WHERE gnpold IS NOT NULL AND gnp IS NOT NULL;
 
 -- 17. The average population of cities in each country (hint: use city.countrycode)
 -- ordered from highest to lowest.
 -- (highest avg population: 4017733.0000, "SGP")
+
 	
 -- 18. The count of cities in each state in the USA, ordered by state name.
 -- (45 rows)
