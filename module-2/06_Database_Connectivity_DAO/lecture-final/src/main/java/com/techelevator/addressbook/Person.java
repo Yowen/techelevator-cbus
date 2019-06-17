892aefd4ca59bdd5e4ccc49987ebc264db64fc02
Brian Lauvray <lauvray.1@osu.edu>
2019-06-17 19:21:40 -0400

    Added Jdbc Phone DAO

--- module-2/06_Database_Connectivity_DAO/lecture-final/src/main/java/com/techelevator/addressbook/Person.java
+++ module-2/06_Database_Connectivity_DAO/lecture-final/src/main/java/com/techelevator/addressbook/Person.java
@@ -6,6 +6,16 @@ public class Person {
 	private String firstName;
 	private String lastName;
 	
+	public Person( ) {
+		
+	}
+	
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	
 	public int getId() {
 		return id;
 	}
