74a22ca03a2a4af9b6143b78690cf09f8401e279
Andrew Frank <andrew@techelevator.com>
2019-08-02 11:00:47 -0400

    Add endpoint for product reviews

---
+++ module-4/13_Creating_APIs/lecture-final/product-reviews-java/ProductReviewsAPI/src/main/java/com/techelevator/reviews/controller/ProductReviewsController.java
@@ -0,0 +1,27 @@
+package com.techelevator.reviews.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techelevator.reviews.dao.IProductReviewDao;
+import com.techelevator.reviews.model.ProductReview;
+
+@RestController
+@RequestMapping("/api/reviews")
+public class ProductReviewsController {
+
+	private final IProductReviewDao productReviewDao;
+
+	public ProductReviewsController(IProductReviewDao productReviewDao) {
+		this.productReviewDao = productReviewDao;
+	}
+
+	@GetMapping
+	public List<ProductReview> getProductReviews() {
+		return productReviewDao.list();
+	}
+
+}
