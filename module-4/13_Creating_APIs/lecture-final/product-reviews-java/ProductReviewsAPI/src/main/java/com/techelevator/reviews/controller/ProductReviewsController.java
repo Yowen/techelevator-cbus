6d0f00a95bc47ef59cd889461cf81ac8740359ee
Andrew Frank <andrew@techelevator.com>
2019-08-02 11:34:55 -0400

    Add API endpoint to update product review

--- module-4/13_Creating_APIs/lecture-final/product-reviews-java/ProductReviewsAPI/src/main/java/com/techelevator/reviews/controller/ProductReviewsController.java
+++ module-4/13_Creating_APIs/lecture-final/product-reviews-java/ProductReviewsAPI/src/main/java/com/techelevator/reviews/controller/ProductReviewsController.java
@@ -2,12 +2,15 @@ package com.techelevator.reviews.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponents;
@@ -52,4 +55,15 @@ public class ProductReviewsController {
 		return ResponseEntity.created(uriComponent.toUri()).body(productReview);
 	}
 
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void updateProductReview(@PathVariable int id, @RequestBody ProductReview productReview) {
+
+		if (productReviewDao.read(productReview.getId()) != null) {
+			productReviewDao.update(productReview);
+			return;
+		}
+
+		throw new ProductReviewNotFoundException(id, "Product review could not be found.");
+	}
 }
