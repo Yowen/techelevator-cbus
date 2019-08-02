e8af7155991e49e3b4a7000ea9abeb0536c53bf1
Andrew Frank <andrew@techelevator.com>
2019-08-02 12:46:04 -0400

    Add CORS support to the Product Review API

--- module-4/13_Creating_APIs/lecture-final/product-reviews-java/ProductReviewsAPI/src/main/java/com/techelevator/reviews/controller/ProductReviewsController.java
+++ module-4/13_Creating_APIs/lecture-final/product-reviews-java/ProductReviewsAPI/src/main/java/com/techelevator/reviews/controller/ProductReviewsController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.techelevator.reviews.model.ProductReview;
 
 @RestController
 @RequestMapping("/api/reviews")
+@CrossOrigin
 public class ProductReviewsController {
 
 	private final IProductReviewDao productReviewDao;
